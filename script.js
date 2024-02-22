b2985702a793d73c32ed395235eb71daa097953d
YoujunZhang <144183143+YoujunZhang@users.noreply.github.com>
2024-02-22 18:12:27 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+function mouseDown() {
+    document.getElementById("one").style.color = "transparent";
+    document.getElementById("one").style.textShadow = "0 0 3px rgb(202, 177, 35)";
+
+  }
+  
+  function mouseUp() {
+    document.getElementById("one").style.color = "gold";
+    document.getElementById("one").style.opacity = 0.55;
+  }
+
+
+
+
+function myFunction(element, clr) {
+    element.style.color = clr;
+}
